e4549d42c1afd1d8699437f62fa48523f2d4a5fe
anupama <anupamagadde@yahoo.com>
2020-02-18 06:50:38 -0600

    no changes made

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,19 +16,21 @@ class ApplicationController < Sinatra::Base
   
   helpers do 
 
-    def logged_in?
-        !!current_user
-    end
-
     def current_user
       @current_user ||= User.find_by(id: session[:user_id])
     end
+
+    def logged_in?
+      !!current_user
+    end
+
     def redirect_if_not_logged_in
       if !logged_in?
         flash[:errors] = "You must be logged in to view the page you tried to view."
         redirect '/'
       end
     end
+    
     def redirect_if_logged_in
       if logged_in?
         redirect "/book_reviews"
